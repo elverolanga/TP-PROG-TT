360f28831c2530540b461e8f50b5f237aecfa147
Martin Langa <167089903+elverolanga@users.noreply.github.com>
2025-06-05 01:06:50 +0200

    idkk

--- src/parking_lot/app/Menu.java
+++ src/parking_lot/app/Menu.java
@@ -1,7 +1,23 @@
 package parking_lot.app;
 
+import parking_lot.entidades.Park;
+
+import java.util.Scanner;
+
 public class Menu {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println(" => Bem-Vindo ao Parque de Estacionamento <= ");
+
+        switch(option){
+            case 1  -> Park.registrarVeiculo();
+            case 2 -> Park.listarVeiculos();
+            case 3 -> {
+                Park.listarVeiculos();
+                System.out.println("Insira o id do veiculo que deseja apagar");
+                int id = Integer.parseInt(sc.nextLine());
+                Park.removerVeiculo(id);
+            }
+        }
     }
 }
