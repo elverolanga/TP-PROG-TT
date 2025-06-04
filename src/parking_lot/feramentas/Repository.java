85a9091977facf8aeaa3bb6ca3473279fade4dba
Martin Langa <167089903+elverolanga@users.noreply.github.com>
2025-06-04 21:51:47 +0200

    feat: db connection

--- src/parking_lot/feramentas/Repository.java
+++ src/parking_lot/feramentas/Repository.java
@@ -1,11 +1,23 @@
 package parking_lot.feramentas;
 
-import parking_lot.entidades.Vaga;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Repository {
+    public static Connection getConnection() {
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/parking_lot";
+        String user = "root";
+        String password = "lanx3ry";
+        try{
+            con = DriverManager.getConnection(url, user, password);
+            con.close();
+        }catch (SQLException e){
+            System.out.println("Erro ao conectar: " + e.getMessage());;
+        }
+        return con;
+    }
+
 
 }
