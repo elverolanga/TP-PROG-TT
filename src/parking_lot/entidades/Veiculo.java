318305a13edeeb7b3eb05455e8faf43873872d6c
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-06-04 22:05:19 +0200

    feat: Classe Veiculo in Progress.

--- src/parking_lot/entidades/Veiculo.java
+++ src/parking_lot/entidades/Veiculo.java
@@ -1,5 +1,19 @@
 package parking_lot.entidades;
 
 public class Veiculo {
+    private String matricula;
+    private String modelo;
+    private int id;
+
+    public Veiculo(String matricula, String modelo, int id){
+        this.matricula = matricula;
+        this.id = id;
+        this.modelo = modelo;
+    }
+    public Veiculo(){}
+
+    public String toString(){
+        return "Matricula: " + matricula + " Modelo: " + modelo + " ID: " + id;
+    }
 
 }
