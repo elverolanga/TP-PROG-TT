6a2913e69ec2e9f321421eb67485da6064d7b60a
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-06-05 01:21:25 +0200

    feat(Menu): Switch_Case01.

--- src/parking_lot/app/Menu.java
+++ src/parking_lot/app/Menu.java
@@ -7,17 +7,31 @@ import java.util.Scanner;
 public class Menu {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println(" => Bem-Vindo ao Parque de Estacionamento <= ");
 
+        System.out.println(" => Bem-Vindo ao Parque de Estacionamento <= ");
+        System.out.println(" => Escolha Uma opcao: <= ");
+        System.out.println(" 1: Adicionar um veiculo; ");
+        System.out.println(" 2: Listar Veiculos; ");
+        System.out.println(" 3: Remover Veiculo; ");
+        System.out.println(" 0: Sair do Parque; ");
+
+        int option = 0;
         switch(option){
-            case 1  -> Park.registrarVeiculo();
-            case 2 -> Park.listarVeiculos();
-            case 3 -> {
+            case 1:
+                Park.registrarVeiculo();
+            case 2:
                 Park.listarVeiculos();
-                System.out.println("Insira o id do veiculo que deseja apagar");
+            case 3:
+                {
+                Park.listarVeiculos();
+                System.out.println("Insira o id do veiculo que deseja remover");
                 int id = Integer.parseInt(sc.nextLine());
                 Park.removerVeiculo(id);
-            }
+                }
+            case 0:
+                // Comando pra sair
+            default:
+                System.out.println("ERROR.");
         }
     }
 }
