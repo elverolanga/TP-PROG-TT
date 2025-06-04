16cd7d5b3c0436d45c9a1f956a13b5d06e974a17
Martin Langa <167089903+elverolanga@users.noreply.github.com>
2025-06-04 23:41:21 +0200

    fix: auto incrementable id's

--- src/parking_lot/entidades/Park.java
+++ src/parking_lot/entidades/Park.java
@@ -2,11 +2,18 @@ package parking_lot.entidades;
 import parking_lot.feramentas.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Park {
 
     public static void eliminarCliente(int id){
+        Repository.delete(id);
+    }
 
+    public static void main(String[] args) {
+
+        Veiculo v1 = new Veiculo("toyota", "corolla", "toyo");
+        Vaga vaga = new Vaga();
     }
 }
