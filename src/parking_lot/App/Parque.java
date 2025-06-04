a85f14ae2ac9da8272504e05ea55a3e576f9c545
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-06-05 00:52:25 +0200

    feat:xs.

---
+++ src/parking_lot/App/Parque.java
@@ -0,0 +1,7 @@
+package parking_lot.App;
+import java.util.Scanner;
+public class Parque {
+    public static void main(String[] args) {
+        System.out.println(" => Bem-Vindo ao Parque de Estacionamento <= ");
+    }
+}
