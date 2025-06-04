eff65ba4b7abd6a96bcab3b3b3d33ab319828f32
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-06-04 22:14:56 +0200

    feat:Veiculo_Get&Set

--- src/parking_lot/entidades/Veiculo.java
+++ src/parking_lot/entidades/Veiculo.java
@@ -12,6 +12,30 @@ public class Veiculo {
     }
     public Veiculo(){}
 
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String toString(){
         return "Matricula: " + matricula + " Modelo: " + modelo + " ID: " + id;
     }
